717daff21259d3971db14571183a40825b72ac53
Marc <marcsr03@gmail.com>
2021-05-20 18:14:01 +0200

    added checkbox and header changes

--- src/App.js
+++ src/App.js
@@ -1,54 +1,110 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-import React from "react";
+// import React, { useState } from "react";
+// import Checkbox from "./components/Checkbox";
+// import hero from "./img/hero.jpg";
+// import "./header.scss";
+// import "./main.scss";
+
+// function App() {
+//   const [checked, setChecked] = useState(false);
+//   return (
+//     <>
+//       <header>
+//         <div className="heroImg" alt="hero" src={hero}>
+//           <h2 className="TODO__Header">TODO</h2>
+//           <form className="TODO__Form">
+//             {/* <input className="mx-4 TODO__Checkbox" type="checkbox" name="new" /> */}
+//             <Checkbox
+//               checked={checked}
+//               onChange={(value) => setChecked(value)}
+//             />
+//             <input
+//               className="TODO__New"
+//               type="text"
+//               placeholder="Create task"
+//             />
+//           </form>
+//         </div>
+//       </header>
+
+//       <main className="main container ">
+//         <section className="row">
+//           <div className="main__todo col col-12">
+//             <label>
+//               <input className="mx-2" type="checkbox" name="name" />
+//               Jog around the park
+//             </label>
+//           </div>
+//           <div className="main__todo col col-12">
+//             <label>
+//               <input className="mx-4" type="checkbox" name="name" />
+//               Jog around the park
+//             </label>
+//           </div>
+//           <div className="main__todo col col-12">
+//             <label>
+//               <input className="mx-4" type="checkbox" name="name" />
+//               Jog around the park
+//             </label>
+//           </div>
+//         </section>
+//         <section className="row main__footer">
+//           <span>5 items left</span>
+//           <div>
+//             <button type="button">All</button>
+//             <button type="button">Active</button>
+//             <button type="button">Completed</button>
+//           </div>
+//           <button type="button">Clear completed</button>
+//         </section>
+//       </main>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+// import TodoList from "./components/TodoList";
+import Checkbox from "./components/Checkbox";
 import hero from "./img/hero.jpg";
 import "./header.scss";
-import "./main.scss";
 
-function App() {
-  return (
-    <>
-      <header>
-        <div className="heroImg" alt="hero" src={hero}>
-          <h1 className="TODOHeader">TODO</h1>
-          <form>
-            <input type="text" placeholder="Create task" />
-          </form>
-        </div>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+    };
+    // this.handleAddTodo = this.handleAddTodo.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-      <main className="main container ">
-        <section className="row">
-          <div className="main__todo col col-12">
-            <label>
-              <input className="mx-4" type="checkbox" name="name" />
-              Jog around the park
-            </label>
+  handleChange() {
+    const { checked } = this.state;
+    this.setState({ checked: !checked });
+  }
+  // handleAddTodo(value) {
+
+  // }
+
+  render() {
+    const { checked } = this.state;
+
+    return (
+      <>
+        <header>
+          <div className="heroImg" alt="hero" src={hero}>
+            <h1 className="TODO__Header">TODO</h1>
+            <form>
+              <Checkbox handleChange={this.handleChange} checked={checked} />
+            </form>
           </div>
-          <div className="main__todo col col-12">
-            <label>
-              <input className="mx-4" type="checkbox" name="name" />
-              Jog around the park
-            </label>
-          </div>
-          <div className="main__todo col col-12">
-            <label>
-              <input className="mx-4" type="checkbox" name="name" />
-              Jog around the park
-            </label>
-          </div>
-        </section>
-        <section className="row main__footer">
-          <span>5 items left</span>
-          <div>
-            <button type="button">All</button>
-            <button type="button">Active</button>
-            <button type="button">Completed</button>
-          </div>
-          <button type="button">Clear completed</button>
-        </section>
-      </main>
-    </>
-  );
+        </header>
+
+        {/* <TodoList todos={todos} /> */}
+      </>
+    );
+  }
 }
 
 export default App;
